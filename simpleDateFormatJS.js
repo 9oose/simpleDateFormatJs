45cdeae842290665821b66a9ec19589e7e94b08a
HeeGoo <9oose.public@gmail.com>
2018-12-05 13:13:37 +0900

    simpleDateFormatJs.js
    First Commit

---
+++ simpleDateFormatJS.js
@@ -0,0 +1,31 @@
+let date = new Date();
+
+const simpleDateFormat = (inputDate) => {
+    let year    = inputDate.getFullYear(),
+        month   = '' + (inputDate.getMonth()+1),
+        day     = '' + inputDate.getDate(),
+        hour    = '' + inputDate.getHours(),
+        minute  = '' + inputDate.getMinutes(),
+        second  = '' + inputDate.getSeconds()
+
+    month   = month.length < 2 ? '0' + month : month;
+    day     = day.length < 2 ? '0' + day : day;
+    hour    = hour.length < 2 ? '0' + hour : hour;
+    minute  = minute.length < 2 ? '0' + minute : minute;
+    second  = second.length < 2 ? '0' + second : second;
+
+    const rtnDate = `${year}-${month}-${day} ${hour}:${minute}:${seconds}`;
+
+    return rtnDate;
+}
+
+const timeCalculator = (type, inputDate, data) =>{
+    switch (type.toUpperCase()){
+        case 'YY' : inputDate.setFullYear(inputDate.getFullYear() + data);break;
+        case 'MM' : inputDate.setMonth(inputDate.getMonth() + data);break;
+        case 'DD' : inputDate.setDate(inputDate.getDate() + data);break;
+        case 'HH' : inputDate.setHours(inputDate.getHours() + data);break;
+        case 'MI' : inputDate.setMinutes(inputDate.getMinutes() + data);break;
+        case 'SS' : inputDate.setSeconds(inputDate.getSeconds() + data);break;
+    }
+}
